2a8b431f2f5b5a255bdf646d3bd37f4dc99a189d
gabriela.izabel.donato.de.souza <gabriela.izabel.donato.de.souza@consultant.volvo.com>
2025-03-08 21:10:38 -0300

    connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,26 @@
-#!/usr/bin/python
+import pygame
+
+from code.menu import Menu
+
+
+# !/usr/bin/python
 # -*- coding: utf-8 -*-
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.screen = pygame.display.set_mode(size=(600, 480))
 
-    def run(self, ):
-        pass
+    def run(self,):
+        while True:
+            menu = Menu(self.screen)
+            menu.run()
+            pass
+
+
+
+    # check for all events
+    # for event in pygame.event.get():
+    #   if event.type == pygame.QUIT:
+    #      pygame.quit()  # close screen
+    #     quit()  # End pygame
