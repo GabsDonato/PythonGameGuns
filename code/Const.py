f6a7cd31828ba565fb186e607df8bb17f8beb894
gabriela.izabel.donato.de.souza <gabriela.izabel.donato.de.souza@consultant.volvo.com>
2025-03-11 11:06:31 -0300

    Finalizando factory and parallax do background nivel1

--- code/Const.py
+++ code/Const.py
@@ -5,6 +5,17 @@ COLOR_YELLOW = (255, 249, 109)
 COLOR_BLUE = (0, 0, 109)
 COLOR_ORANGE = (255, 111, 0)
 
+
+# E
+ENTITY_SPEED = {
+    'Level1BG0': 0,
+    'Level1BG1': 1,
+    'Level1BG2': 2,
+    'Level1BG3': 3,
+    'Level1BG4': 4,
+    'Level1BG5': 5,
+}
+
 #MENU
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
--- code/game.py
+++ code/game.py
@@ -1,6 +1,7 @@
 import pygame
 
-from code.Const import SCR_WIDTH, SCR_HEIGHT
+from code.Const import SCR_WIDTH, SCR_HEIGHT, MENU_OPTION
+from code.level import Level
 from code.menu import Menu
 
 
@@ -12,12 +13,16 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode(size=(SCR_WIDTH, SCR_HEIGHT))
 
-    def run(self,):
+    def run(self, ):
         while True:
             menu = Menu(self.screen)
-            menu.run()
-            pass
+            menu_return = menu.run()
 
-
-
-
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.screen, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
